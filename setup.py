0e4e6208a6bfcc8587f10b1d1e78aa9e7477f289
HowieHye <howiehye@163.com>
2020-12-10 19:49:39 +0800

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     description="SDK for qqpusher",
     url='https://github.com/howiehye/qqpusher.git',
     long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
     platforms=["all"],
     license="GPLv3",
     classifiers=[
