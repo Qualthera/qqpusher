38f69d0d4f02b5d2032ee13a779c363ddd9e08fb
HowieHye <howiehye@163.com>
2020-12-28 16:31:09 +0800

    Bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import (setup, find_packages)
 
 setup(
     name="qqpusher",
-    version="0.1.6",
+    version="0.1.7",
     description="SDK for qqpusher",
     author='Qualthera',
     author_email='qualthera@163.com',
